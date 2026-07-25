fa0bd927b23b630b35959f6c81bc39c210efd456
paulaisbrecht <paula.isbrecht@student.htw-berlin.de>
2026-07-25 12:19:26 +0200

    Introduce Bank Account Controller integration test error

--- modules/bank-account/src/main/kotlin/com/bank/bankaccount/api/BankAccountController.kt
+++ modules/bank-account/src/main/kotlin/com/bank/bankaccount/api/BankAccountController.kt
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/accounts")
+@RequestMapping("/api/account")
 class BankAccountController(
     private val bankAccountService: BankAccountService,
 ) {
